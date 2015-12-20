6ab8b50d3ed23d775ae1bcef709fa1675dbf6c1c
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:59:48 -0800

    fix 2015-12-18 toolchain SPM bug

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Sideburns",
     dependencies: [
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
 		.Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 1)
     ]
 )
