e9db65e4e5565386fadece4335b3d9175e4f869c
Anton <antonmes@users.noreply.github.com>
2016-04-19 17:56:34 +0400

    Snapshot 2016 04 12 (#3)
    * Migrate to April 12, 2016 snapshot
    
    * use readAll
    
    * readAllBytes

--- Sources/Sideburns.swift
+++ Sources/Sideburns.swift
@@ -37,16 +37,16 @@ extension Response {
     public init(status: Status = .ok, headers: Headers = [:], templatePath: String, templateData: TemplateData) throws {
         let templateFile = try File(path: templatePath, mode: .read)
 
-        guard let templateString = try? String(data: templateFile.read()) else {
+        guard let templateString = try? String(data: templateFile.readAllBytes()) else {
             throw SideburnsError.unsupportedTemplateEncoding
         }
 
         let template = try Template(string: templateString)
-        let rendering = try template.render(Box(boxable: templateData))
+        let rendering = try template.render(box: Box(boxable: templateData))
 
         self.init(status: status, headers: headers, body: rendering)
 
-        if let fileExtension = templateFile.fileExtension, mediaType = mediaTypeForFileExtension(fileExtension) {
+        if let fileExtension = templateFile.fileExtension, mediaType = mediaType(forFileExtension: fileExtension) {
             self.contentType = mediaType
         }
     }
