5fd5f1cfb31314992108156cc188d7e869c40206
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-25 01:52:24 -0300

    add tests

---
+++ Tests/Sideburns/SideburnsTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Sideburns
+
+class SideburnsTests: XCTestCase {
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
+}
+
+extension SideburnsTests {
+    static var allTests : [(String, SideburnsTests -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+}
