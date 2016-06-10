5954bba926922b2c24b6b28005e4d07a42072a8e
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2016-06-11 05:00:36 +0900

    Update for Snapshot 05-09 (#5)
    only dependencies update needed

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Sideburns",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
     ]
 )
