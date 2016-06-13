6ee735abc4517a9cf0a5918fdfe5cda1782f487a
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-06-13 18:45:25 +0900

    updated http version
    worked fine

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Sideburns",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
     ]
 )
