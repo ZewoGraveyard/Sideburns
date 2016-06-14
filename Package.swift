1344aef11df698d7c9f4f006096d7c8a6301328d
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-06-14 11:38:01 +0900

    Bump to 0.8

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Sideburns",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
     ]
 )
